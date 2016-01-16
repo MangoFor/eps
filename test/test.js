ebd47dd6a4343721188a0600d58f6f64e58edecb
mango <mangotree@aliyun.com>
2016-01-16 14:58:40 +0800

    package router and add path to regexp module,enable app.use match prefix path

--- test/test.js
+++ test/test.js
@@ -5,22 +5,25 @@ var express = require('../index.js');
 
 describe('application', function() {
   describe('use', function() {
-    it('app.use can add new route', function(done){
-    	var app = express();
-    	app.use('/user',function(req,res){
-    		res.end('/user')
-    	})
-    	app.use('/index',function(req,res){
-    		res.end('/index')
-    	})
 
+    var app = express();    
+    app.use('/user',function(req,res){
+        res.end('/user')
+    })
+    app.use('/index',function(req,res){
+        res.end('/index')
+    })
+
+    it('app.use can add new route', function(done){
     	request(app)
     	.get('/user')
     	.expect(200,'/user',done);
-
-    	request(app)
-    	.get('/index')
-    	.expect(200,'/user',done);
-		})
 	})
+
+    it('app.use will match prefix path', function(done){        
+         request(app)
+        .get('/user/kkk')
+        .expect(200,'/user',done);
+    })
+  })
 })
